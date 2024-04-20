f6f86e1721ea5ea8d11b47175e1e03b8f1df928b
Xeon0X <xeon0x.gaming@gmail.com>
2024-04-20 15:49:30 +0200

    Fix main

--- main.py
+++ main.py
@@ -3,15 +3,13 @@ import networks.curve as curve
 
 
 
-# editor = Editor(buffering=True)
+editor = Editor(buffering=True)
 
-# # Get a block
-# block = editor.getBlock((0,48,0))
+# Get a block
+block = editor.getBlock((0,48,0))
 
-# # Place a block
-# editor.placeBlock((394, 132, 741), Block("stone"))
+# Place a block
+editor.placeBlock((394, 132, 741), Block("stone"))
 
-# # Build a cube
-# geometry.placeCuboid(editor, (458, 92, 488), (468, 99, 471), Block("oak_planks"))
-
-curve = curve.Curve([(0, 0, 0)])
+# Build a cube
+geometry.placeCuboid(editor, (458, 92, 488), (468, 99, 471), Block("oak_planks"))
