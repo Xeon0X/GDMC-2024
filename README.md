aaf72e780691fcf9ee90f69cb9b81dff3df2247d
Xeon0X <xeon0x.gaming@gmail.com>
2024-04-20 12:58:20 +0200

    Add run instructions and requirements

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # GDMC-2024
 A procedural city generator for Minecraft as part of the GDMC 2024 competition. 
+
+# Run
+
+Install required packages using `pip`:
+```python
+pip install -r requirements.txt
+```
+
+Run `main.py`.
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ editor = Editor(buffering=True)
 block = editor.getBlock((0,48,0))
 
 # Place a block
-editor.placeBlock((448, 92, 515), Block("stone"))
+editor.placeBlock((394, 132, 741), Block("stone"))
 
 # Build a cube
 geometry.placeCuboid(editor, (458, 92, 488), (468, 99, 471), Block("oak_planks"))
