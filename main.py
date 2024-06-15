919b2459740f792d13bf0008551a427abd428085
NichiHachi <kevin.angelier@aol.fr>
2024-06-15 22:32:15 +0200

    OOPSI

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from world_maker.world_maker import *
 
 def main():
-    world_maker()
+    rectangle_house_mountain, rectangle_building, skeleton_highway, skeleton_mountain = world_maker()
 
 if __name__ == '__main__':
-    rectangle_house_mountain,rectangle_building, skeleton_highway, skeleton_mountain = main()
+    main()
